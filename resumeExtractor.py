acd1c22b6510c5304d6bbd6c29c9bff34f3af144
Vaibhav Ankolekar <76490924+RagnosVA@users.noreply.github.com>
2022-09-02 16:07:41 +0530

    extract_mobile_number completed

--- resumeExtractor.py
+++ resumeExtractor.py
@@ -60,7 +60,14 @@ class resumeExtractor:
 			return span.text
 	
 	def __extract_mobile_number(self, text):
-		pass
+		# get list of numbers in text
+		phone = re.findall(re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
+		if phone:
+			number = ''.join(phone[0])
+			if len(number) > 10:
+				return '+' + number
+			else:
+				return number
 	
 	def __extract_email(self, text):
 		pass
