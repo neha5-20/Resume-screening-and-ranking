381bd1c2ddf9695b342b5ac3230506ae68958e6f
neha5-20 <nehachogale795@gmail.com>
2022-10-13 20:45:54 +0530

    css added

--- static/js/applicant_dashboard.js
+++ static/js/applicant_dashboard.js
@@ -1,28 +1,28 @@
-// const form = document.querySelector("form"),
-// fileInput = document.querySelector(".upload-button"),
-// progressArea = document.querySelector(".progress-area");
-// form.addEventListener("click", () =>{
-//   fileInput.click();
-// });
+const form = document.querySelector(".icon"),
+fileInput = document.querySelector(".input-file"),
+progressArea = document.querySelector(".progress-area");
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
 
-// fileInput.onchange = ({target})=>{
-//     let file = target.files[0];
-//     if(file){
-//       let fileName = file.name;
-//       uploadFile(fileName)
-//     }
-//   }
+fileInput.onchange = ({target})=>{
+    let file = target.files[0];
+    if(file){
+      let fileName = file.name;
+      uploadFile(fileName)
+    }
+  }
 
-//  function uploadFile(name) {
-//     let progressHTML = `<li class="applicant_row">
-//                             <i class="fas fa-file-alt"></i>
-//                             <div class="applicant_content>
-//                                 <div class="up_file_details">
-//                                     <span class="up_file_name">${name} • Uploaded</span>
-//                                 </div>
-//                             </div>
-//                             <i class="fas fa-check"></i>
-//                         </li>`;
+ function uploadFile(name) {
+    let progressHTML = `<li class="applicant_row">
+                            <i class="fas fa-file-alt"></i>
+                            <div class="applicant_content>
+                                <div class="up_file_details">
+                                    <span class="up_file_name">${name} • Uploaded</span>
+                                </div>
+                            </div>
+                            <i class="fas fa-check"></i>
+                        </li>`;
 
-//     progressArea.innerHTML = progressHTML;
-//  }
+    progressArea.innerHTML = progressHTML;
+ }
